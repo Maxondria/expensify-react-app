117691799bd9c83c1db79021f43f63dbb10ddd05
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-10-03 18:27:11 +0300

    [task] filter reducer and action creators

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -3,7 +3,9 @@ import uuid from "uuid";
 
 const actionTypes = {
   ADD_EXPENSE: "ADD_EXPENSE",
-  REMOVE_EXPENSE: "REMOVE_EXPENSE"
+  REMOVE_EXPENSE: "REMOVE_EXPENSE",
+  EDIT_EXPENSE: "EDIT_EXPENSE",
+  SET_TEXT_FILTER: "SET_TEXT_FILTER"
 };
 
 const addExpense = ({
@@ -27,6 +29,16 @@ const removeExpense = ({ id } = {}) => ({
   id
 });
 
+const editExpense = (id, expense) => ({
+  type: actionTypes.EDIT_EXPENSE,
+  expense
+});
+
+const setTextFilter = (text = "") => ({
+  type: actionTypes.SET_TEXT_FILTER,
+  text
+});
+
 const initialState = {
   expenses: [],
   filters: {
@@ -43,6 +55,14 @@ const expensesReducer = (state = initialState.expenses, action) => {
       return [...state, action.expense];
     case actionTypes.REMOVE_EXPENSE:
       return state.filter(expense => expense.id !== action.id);
+    case actionTypes.EDIT_EXPENSE:
+      return state.map(expense => {
+        if (expense.id === action.id) {
+          return { ...state, ...action.expense };
+        } else {
+          return expense;
+        }
+      });
     default:
       return state;
   }
@@ -50,6 +70,8 @@ const expensesReducer = (state = initialState.expenses, action) => {
 
 const filtersReducer = (state = initialState.filters, action) => {
   switch (action.type) {
+    case actionTypes.SET_TEXT_FILTER:
+      return { ...state, text: action.text };
     default:
       return state;
   }
