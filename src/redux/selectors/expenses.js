b8996fdfb171f19896390d02d0ec81663fb6c8d6
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-10-04 20:04:25 +0300

    [task] add moment package and refactor expenses selector to use moment

--- src/redux/selectors/expenses.js
+++ src/redux/selectors/expenses.js
@@ -1,16 +1,22 @@
+import moment from "moment";
+
 export default (expenses, filters) => {
   const { text, sortBy, startDate, endDate } = filters;
   return expenses
     .filter(expense => {
-      const startDateMatch =
-        typeof startDate !== "number" || expense.createdAt >= startDate;
-      const endDateMatch =
-        typeof endDate !== "number" || expense.createdAt <= endDate;
+      const createdAtMoment = moment(expense.createdAt);
+      const startDateMatch = startDate
+        ? startDate.isSameOrBefore(createdAtMoment, "day")
+        : true;
+      const endDateMatch = endDate
+        ? endDate.isSameOrAfter(createdAtMoment, "day")
+        : true;
       const textMatch = expense.description
         .toLowerCase()
         .includes(text.toLowerCase());
+
       return startDateMatch && endDateMatch && textMatch;
-    })
+    }) // eslint-disable-next-line
     .sort((a, b) => {
       if (sortBy === "date") {
         return a.createdAt < b.createdAt ? 1 : -1;
