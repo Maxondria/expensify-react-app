422e7a525799fb9c7af788701c34fa7cc6b99cbd
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-10-04 19:52:39 +0300

    [task] use a selector to get filtered and sorted expenses

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
 import ExpenseListItem from "./ExpenseListItem";
+import selectExpenses from "../redux/selectors/expenses";
 
 const ExpenseList = props => (
   <div>
     <h1>This is my Expenses List</h1>
     {props.expenses.map(expense => (
-      <ExpenseListItem {...expense} />
+      <ExpenseListItem key={expense.id} {...expense} />
     ))}
   </div>
 );
 
 const mapStateToProps = ({ expenses, filters }) => ({
-  expenses,
+  expenses: selectExpenses(expenses, filters),
   filters
 });
 
