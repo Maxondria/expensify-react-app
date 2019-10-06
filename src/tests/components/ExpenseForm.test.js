ec5a4855daa65ac3060a03dbbdbaf90368e74972
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-10-07 00:03:28 +0300

    [test] expense form user input and errors

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -12,3 +12,53 @@ it("should render ExpenseForm with props - edit", function() {
   const wrapper = shallow(<ExpenseForm expense={expenses[0]} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+it("should render error for invalid form submission", function() {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+  wrapper.find("form").simulate("submit", { preventDefault: jest.fn() });
+  expect(wrapper.state().error).toEqual("Amount and Description Required");
+  expect(wrapper).toMatchSnapshot();
+});
+
+it("should set description state property to user input on change", function() {
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper
+    .find("input")
+    .at(0)
+    .simulate("change", {
+      target: {
+        name: "description",
+        value: "description"
+      }
+    });
+  expect(wrapper.state().description).toEqual("description");
+});
+
+it("should reject amount user input on change if it's not a number", function() {
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper
+    .find("input")
+    .at(1)
+    .simulate("change", {
+      target: {
+        name: "amount",
+        value: "WILL-BE-REJECTED"
+      }
+    });
+  expect(wrapper.state().amount).toEqual("");
+});
+
+it("should set amount state property to user input if it's a number", function() {
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper
+    .find("input")
+    .at(1)
+    .simulate("change", {
+      target: {
+        name: "amount",
+        value: "345.56"
+      }
+    });
+  expect(wrapper.state().amount).toEqual("345.56");
+});
