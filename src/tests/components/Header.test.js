aea750027f6985c56e435444b49d542870c3587e
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-10-06 21:17:19 +0300

    [test] use enzyme to test header instead

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,9 @@
-import ReactShallowRenderer from "react-test-renderer/shallow";
+import { shallow } from "enzyme";
 import React from "react";
 import Header from "../../components/Header";
 
 it("should render Header correctly", function() {
-  const renderer = new ReactShallowRenderer();
-
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(wrapper.find("h1").length).toBe(1);
+  expect(wrapper.find("h1").text()).toBe("Expensify");
 });
