93bf34dd02088ffe588812f77667032a79d0db95
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-10-06 22:14:19 +0300

    [test] configure jest-enzyme-to-json snapshot serializers

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -6,4 +6,5 @@ it("should render Header correctly", function() {
   const wrapper = shallow(<Header />);
   expect(wrapper.find("h1").length).toBe(1);
   expect(wrapper.find("h1").text()).toBe("Expensify");
+  expect(wrapper).toMatchSnapshot();
 });
