63743e926d3b99bf7669fefce86af8f362f9e80a
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-10-07 16:46:40 +0300

    [test] refactor edit expense page and test it

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -2,34 +2,41 @@ import React from "react";
 import { connect } from "react-redux";
 import ExpenseForm from "./ExpenseForm";
 import { editExpense, removeExpense } from "../redux/actions/expenses";
-import { Redirect } from "react-router-dom";
 
-const EditExpensePage = props => {
-  const id = props.match.params.id;
-  return (
-    <div>
-      {!props.expense && <Redirect to="/" />}
+export class EditExpensePage extends React.Component {
+  id = this.props.match.params.id;
 
-      <ExpenseForm
-        expense={props.expense}
-        onSubmit={({ note, description, amount, date: createdAt }) => {
-          props.editExpense(id, {
-            note,
-            description,
-            amount,
-            createdAt
-          });
-          props.history.push("/");
-        }}
-      />
-      <button
-        onClick={() => props.removeExpense(id) && props.history.push("/")}
-      >
-        Remove
-      </button>
-    </div>
-  );
-};
+  onSubmit = ({ note, description, amount, date: createdAt }) => {
+    this.props.editExpense(this.id, {
+      note,
+      description,
+      amount,
+      createdAt
+    });
+    this.props.history.push("/");
+  };
+
+  onRemoveExpense = () =>
+    this.props.removeExpense(this.id) && this.props.history.push("/");
+
+  render() {
+    return (
+      <div>
+        {this.props.expense.length === 0 ? (
+          <p>Expense Not Found</p>
+        ) : (
+          <>
+            <ExpenseForm
+              expense={this.props.expense}
+              onSubmit={this.onSubmit}
+            />
+            <button onClick={this.onRemoveExpense}>Remove</button>
+          </>
+        )}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = ({ expenses }, props) => ({
   expense: expenses.find(expense => expense.id === props.match.params.id)
---
+++ src/tests/components/EditExpensePage.test.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { EditExpensePage } from "../../components/EditExpensePage";
+import { shallow } from "enzyme";
+import expenses from "../redux/fixtures/expenses.fixture";
+
+const props = {
+  expense: {
+    note: expenses[0].note,
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    date: expenses[0].createdAt
+  },
+  editExpense: jest.fn(),
+  removeExpense: jest.fn(),
+  match: { params: { id: 1 } },
+  history: {
+    push: jest.fn()
+  }
+};
+
+it("should render EditExpensePage correctly", function() {
+  const wrapper = shallow(<EditExpensePage {...props} />);
+
+  expect(wrapper).toMatchSnapshot();
+});
+
+it("should exit page if no expense was found (Reloading Page with temporary store)", function() {
+  const noExpenseProps = { ...props, expense: [] };
+  const wrapper = shallow(<EditExpensePage {...noExpenseProps} />);
+
+  expect(wrapper).toMatchSnapshot();
+  expect(wrapper.find("p").text()).toBe("Expense Not Found");
+});
+
+it("should handle onSubmit()", function() {
+  const wrapper = shallow(<EditExpensePage {...props} />);
+
+  wrapper.find("ExpenseForm").prop("onSubmit")(props.expense);
+  expect(props.editExpense).toHaveBeenLastCalledWith(props.match.params.id, {
+    note: expenses[0].note,
+    description: expenses[0].description,
+    amount: expenses[0].amount,
+    createdAt: expenses[0].createdAt
+  });
+  expect(props.history.push).toHaveBeenLastCalledWith("/");
+});
+
+it("should remove expense and redirect to homepage if remove button clicked", function() {
+  const wrapper = shallow(<EditExpensePage {...props} />);
+
+  wrapper.find("button").simulate("click");
+  expect(props.removeExpense).toHaveBeenLastCalledWith(props.match.params.id);
+  expect(props.history.push).toHaveBeenLastCalledWith("/");
+});
