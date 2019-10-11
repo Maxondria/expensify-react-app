bc207a264251bdf009d9f4da89c5693cca117a8c
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-10-11 11:17:39 +0300

    [style] style the login page

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -8,8 +8,12 @@ export class LoginPage extends Component {
   };
   render() {
     return (
-      <div>
-        <button onClick={this.Login}>Login</button>
+      <div className="box-layout">
+        <div className="box-layout__box">
+          <h1 className="box-layout__title">Expensify App</h1>
+          <p>It's time to get your expenses under control</p>
+          <button onClick={this.Login}>Login{process.env.PUBLIC_URL}</button>
+        </div>
       </div>
     );
   }
